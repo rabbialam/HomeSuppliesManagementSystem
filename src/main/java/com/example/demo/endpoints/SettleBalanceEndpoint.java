ed5e3a4e320e851a2c6f704bac7153e28700fd3d
Sajid <msajid@uncc.edu>
2019-12-03 02:32:33 -0500

    settleBalance Bari

--- src/main/java/com/example/demo/endpoints/SettleBalanceEndpoint.java
+++ src/main/java/com/example/demo/endpoints/SettleBalanceEndpoint.java
@@ -1,6 +1,8 @@
 package com.example.demo.endpoints;
 
+import com.example.demo.controler.SettleBalanceService;
 import com.example.demo.endpoints.dto.SettleBalanceDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,15 @@ import java.util.Map;
 @RequestMapping("/api")
 public class SettleBalanceEndpoint {
 
+    @Autowired
+    SettleBalanceService settleBalanceService;
+
     @PostMapping
     @RequestMapping("/settleAmount")
     Map<HttpStatus, String> settleAmount(SettleBalanceDTO settleBalanceDTO){
         Map<HttpStatus, String> statusMap = new HashMap<>();
 
-
+        settleBalanceService.settleBalanceFroGroup(settleBalanceDTO);
         statusMap.put(HttpStatus.OK, "Balance settle");
         return statusMap;
 
--- src/main/java/com/example/demo/endpoints/dto/SettleBalanceDTO.java
+++ src/main/java/com/example/demo/endpoints/dto/SettleBalanceDTO.java
@@ -8,5 +8,5 @@ import javax.validation.constraints.NotNull;
 public class SettleBalanceDTO {
     @NotNull
     @NotEmpty
-    private Long groupID;
+    private Long groupId;
 }
